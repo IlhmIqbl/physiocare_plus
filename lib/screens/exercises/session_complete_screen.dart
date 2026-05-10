2fc5f00b0c9a49fb603b6067bd594d0852168646
IlhmIqbl <ilhamiqbal16057@gmail.com>
2026-05-10 22:15:15 +0800

    fix: replace deprecated withOpacity with withValues in SessionCompleteScreen

--- lib/screens/exercises/session_complete_screen.dart
+++ lib/screens/exercises/session_complete_screen.dart
@@ -90,7 +90,7 @@ class _StatRow extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
-              color: Colors.black.withOpacity(0.05), blurRadius: 8),
+              color: Colors.black.withValues(alpha: 0.05), blurRadius: 8),
         ],
       ),
       child: Row(
